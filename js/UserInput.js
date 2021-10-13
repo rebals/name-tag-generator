77e5ff799f543f6e25c1676514a95dfceadb03c1
rebals <rls.blue09@gmail.com>
2021-10-13 12:59:10 -0500

    Initial commit

---
+++ js/UserInput.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class UserInput extends Component {
+  state = { name: "" };
+  updateName = (event) => {
+    this.setState({ name: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({ name: "" });
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Add a new name here..."
+            value={this.state.name}
+            onChange={this.updateName}
+          />
+          <input type="submit" value="Create Name Tag" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default UserInput;
